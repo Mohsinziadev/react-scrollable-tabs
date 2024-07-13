9a1797dfaae573cb221699789c2c70a9d3da3a14
Mohsinziadev <mohsinziadev@gmail.com>
2024-07-14 00:49:03 +0500

    update v3.1  13-07-2024

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ const tabsList = [
                         {/* Set Position of React-Tw-Tabs : */}
                         Set Position of React-Smooth-Tabs :
                     </h3>
-                    <div className="flex ">
+                    <div className="flex  border">
                         <button
                             onClick={() => setSelectedTab("Horizontal")}
                             className={`flex px-4 py-2 rounded-tl-md rounded-bl-md shadow-lg    ${
